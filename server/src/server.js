25c78429d6d484e8c349811a0f057880e3c3526e
LeeJaeLee <cuonggaayf01@gmail.com>
2022-07-18 23:26:32 +0700

    main

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const router = require("./routers");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/blogapp");
 
 app.get("/", (req, res) => {
   res.send("Server");
 });
+
+router(app);
 
 app.listen(3030);
